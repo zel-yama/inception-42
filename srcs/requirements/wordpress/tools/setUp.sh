191544b60d14e41683cec3f60cee66f1af9ef7bf
zel-yama <138831751+zel-yama@users.noreply.github.com>
2026-05-08 11:31:57 +0000

    ss

--- srcs/requirements/wordpress/tools/setUp.sh
+++ srcs/requirements/wordpress/tools/setUp.sh
@@ -1,6 +1,12 @@
 
 
+
 wp config  create  --dbname=$DATABASE_NAME \
           --dbuser=$MYSQL_USER \
           --dbpass=$MYSQL_PASSWORD \
+          --dbhost=mariadb \
+          
+
+wp core install --url=zel-yama.fr.42 --title="the my wordpress site" \
+    --admin_user=$WORDPRESS_ADMIN_USER_NAME --admin_password=$WORDPRESS_PASSWORD  
 
