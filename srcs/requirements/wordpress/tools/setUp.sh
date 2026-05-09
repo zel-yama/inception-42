60710ebda09739b902efdc71a25afa3f1d03fba2
zel-yama <138831751+zel-yama@users.noreply.github.com>
2026-05-09 08:58:43 +0000

    just fix some error

--- srcs/requirements/wordpress/tools/setUp.sh
+++ srcs/requirements/wordpress/tools/setUp.sh
@@ -1,13 +1,21 @@
 
 
+#i should check if data create and 
+#i should check config in first config 
 
 wp config  create  --dbname=$DATABASE_NAME \
           --dbuser=$MYSQL_USER \
           --dbpass=$MYSQL_PASSWORD \
-          --dbhost=mariadb \
+          --dbhost=mariadb --allow-root \
           
 
-wp core install --url=zel-yama.fr.42 --title="the my wordpress site" \
+wp --allow-root core install --url=zel-yama.fr.42 --title="the my wordpress site" \
     --admin_user=$WORDPRESS_ADMIN_USER_NAME --admin_password=$WORDPRESS_PASSWORD  
+
+wp user create $WORDPRESS_USER $WORDPRESS_EMAIL_USER --role=editor --user_pass=$WORDPRESS_USER_PASSWORD
+
+
+
+
 
 
