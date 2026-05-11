70cc5e26cd9772165384e63b308f42b571a06700
zel-yama <138831751+zel-yama@users.noreply.github.com>
2026-05-11 10:02:34 +0000

    ss

--- srcs/requirements/mariadb/tools/set.sh
+++ srcs/requirements/mariadb/tools/set.sh
@@ -1,15 +1,17 @@
 
-service start mariadb
+#!/bin/bash
 
-mariadb -u root -p ${SQL_ROOT_PASSWORD}
-mariadb -e "CREATE DATABASE IF NOT EXISTS `${DATABASE_NAME}`"
+service  mariadb start
 
-mariadb -e "CREATE USER IF NOT EXISTS ${MYSQL_USER}`@``%` IDNTIFIED BY  `${MYSQL_PASSWORD}`"
+mariadb -u root -p${SQL_ROOT_PASSWORD}
+mariadb -e "CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME}"
+
+mariadb -e "CREATE USER IF NOT EXISTS ${MYSQL_USER}@% IDNTIFIED BY  ${MYSQL_PASSWORD}"
 
 mariadb -e " GRANT ALL PRIVILEGES; "
 
 mariadb -e "FLUSH PRIVILEGES;" 
 
-mariadbmyadmin -u root -p ${MYSQL_ROOT_PASSWORD} shutdown 
+mariadbmyadmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown 
 
 exec mariadb_safe  #i should put port and op address 
