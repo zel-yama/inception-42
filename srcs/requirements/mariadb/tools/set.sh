ecf9831da461ffd298f4edbe86676084c1e4cabf
zel-yama <138831751+zel-yama@users.noreply.github.com>
2026-05-12 09:46:57 +0000

    just :

--- srcs/requirements/mariadb/tools/set.sh
+++ srcs/requirements/mariadb/tools/set.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-service  mariadb start
+service mariadb start
 
-mariadb -u root -p${SQL_ROOT_PASSWORD}
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS '${DATABASE_NAME}';"
+mariadb -u root -p${SQL_ROOT_PASSWORD} \
+-e "CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME};"
 
-mariadb -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY  '${MYSQL_PASSWORD}' ;" 
+mariadb -u root -p${SQL_ROOT_PASSWORD} \
+-e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-mariadb -e  "GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;"
+mariadb -u root -p${SQL_ROOT_PASSWORD} \
+-e "GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.* TO '${MYSQL_USER}'@'%';"
 
-mariadb -e "FLUSH PRIVILEGES;" 
+mariadb -u root -p${SQL_ROOT_PASSWORD} \
+-e "FLUSH PRIVILEGES;"
 
-mysqlmyadmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown 
+mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
 
-exec mysql_safe --port=3306 --bind-address=0.0.0.0 #i should put port and op address 
+exec mysqld_safe --port=3306 --bind-address=0.0.0.0
