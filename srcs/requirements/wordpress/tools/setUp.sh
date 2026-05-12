817eaf6deb63d5ed89cb487a802f8369e52f0d57
zel-yama <138831751+zel-yama@users.noreply.github.com>
2026-05-12 09:46:23 +0000

    just

--- srcs/requirements/wordpress/tools/setUp.sh
+++ srcs/requirements/wordpress/tools/setUp.sh
@@ -3,23 +3,33 @@
 #i should check if data create and 
 #i should check config in first config 
 
+#!/bin/bash
 
-wp config  create  --allow-root  --dbname=$DATABASE_NAME \
-          --dbuser=$MYSQL_USER \
-          --dbpass=$MYSQL_PASSWORD \
-          --dbhost=mariadb 
-          
 
-wp --allow-root core install --url=zel-yama.fr.42 --title="the my wordpress site" \
-    --admin_email=$EMAIL_USER_WORDPRESS    \
+
+sleep 10
+
+if [ ! -f wp-config.php ]; then
+
+wp config create --allow-root \
+    --dbname=$DATABASE_NAME \
+    --dbuser=$MYSQL_USER \
+    --dbpass=$MYSQL_PASSWORD \
+    --dbhost=mariadb
+
+wp core install --allow-root \
+    --url=zel-yama.42.fr \
+    --title="the my wordpress site" \
+    --admin_email=$EMAIL_USER_WORDPRESS \
     --admin_user=$WORDPRESS_ADMIN_USER_NAME \
-     --admin_password=$WORDPRESS_PASSWORD  
+    --admin_password=$WORDPRESS_PASSWORD
 
-wp user create --allow-root  $WORDPRESS_USER $WORDPRESS_EMAIL_USER --role=editor --user_pass=$WORDPRESS_USER_PASSWORD
+wp user create --allow-root \
+    $WORDPRESS_USER \
+    $WORDPRESS_EMAIL_USER \
+    --role=editor \
+    --user_pass=$WORDPRESS_USER_PASSWORD
 
+fi
 
-
-php-fpm3.8 -F
-
-
-
+exec php-fpm8.2 -F
