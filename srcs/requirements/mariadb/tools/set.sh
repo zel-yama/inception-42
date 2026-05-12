1bd581265b2cd45fe54db492e773a6ea0fa1479f
zel-yama <138831751+zel-yama@users.noreply.github.com>
2026-05-12 11:23:35 +0000

    just small erorr

--- srcs/requirements/mariadb/tools/set.sh
+++ srcs/requirements/mariadb/tools/set.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-service  mysql start;
+service  mariadb start;
 
-mysql -e "CREATE  IF NOT EXSITS ${DATABASE_NAME}; "
+mysql -e "CREATE  IF NOT EXISTS ${DATABASE_NAME}; "
 mysql -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY ${MYSQL_ROOT_PASSWORD}; "
-mysql -e "GRANT ALL PRIVILEGS ON '${DATABASE_NAME}'.* TO ${MYSQL_USER} IDENTIFIED BY ${MYSQL_ROOT_PASSWORD};"
+mysql -e "GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY ${MYSQL_ROOT_PASSWORD};"
 mysql -e "ALERT USER 'root'@'localhost' IDENTIFIED BY ${MYSQL_ROOT_PASSWORD};"
-mysql -e "FLUSH PRIVILEGS;"
+mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u${MYSQL_USER} -p${MYSQL_ROOT_PASSWORD} shutdown 
 
