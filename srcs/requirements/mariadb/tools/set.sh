6dc8a7d62143a30172af7c785a51a09080eaa0ad
zel-yama <138831751+zel-yama@users.noreply.github.com>
2026-05-12 10:49:32 +0000

    ss

--- srcs/requirements/mariadb/tools/set.sh
+++ srcs/requirements/mariadb/tools/set.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+service  mysql start;
+
+mysql -e "CREATE  IF NOT EXSITS ${DATABASE_NAME}; "
+mysql -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY ${MYSQL_ROOT_PASSWORD}; "
+mysql -e "GRANT ALL PRIVILEGS ON '${DATABASE_NAME}'.* TO ${MYSQL_USER} IDENTIFIED BY ${MYSQL_ROOT_PASSWORD};"
+mysql -e "ALERT USER 'root'@'localhost' IDENTIFIED BY ${MYSQL_ROOT_PASSWORD};"
+mysql -e "FLUSH PRIVILEGS;"
+
+mysqladmin -u${MYSQL_USER} -p${MYSQL_ROOT_PASSWORD} shutdown 
+
+
+exec mysql_safe
