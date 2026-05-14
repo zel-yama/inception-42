75ecb9adb2e2f9334f5c1065cf6cbff5b800e3b2
zel-yama <138831751+zel-yama@users.noreply.github.com>
2026-05-14 13:12:10 +0000

    check incpetion

--- srcs/requirements/mariadb/tools/set.sh
+++ srcs/requirements/mariadb/tools/set.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+
+
 service  mariadb start;
 
+if [-d "/var/lib/mysql/${DATABASE_NAME}"];
+then
+    echo "data base already created" 
+else
 
 mysql -e "CREATE DATABASE  IF NOT EXISTS  ${DATABASE_NAME}; "
 mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'; "
@@ -11,5 +17,5 @@ mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u${MYSQL_USER} -p${MYSQL_ROOT_PASSWORD} shutdown 
 
+fi
 
-exec mysqld_safe
