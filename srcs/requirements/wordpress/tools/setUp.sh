738285b5c0c79a8699f31a630dfa4df5e8b8485f
zel-yama <138831751+zel-yama@users.noreply.github.com>
2026-06-03 15:03:23 +0000

    start vertua

--- srcs/requirements/wordpress/tools/setUp.sh
+++ srcs/requirements/wordpress/tools/setUp.sh
@@ -26,21 +26,23 @@ wp config create --allow-root \
     --dbname=$DATABASE_NAME \
     --dbuser=$MYSQL_USER \
     --dbpass=$MYSQL_PASSWORD \
-    --dbhost=mariadb:3306
+    --dbhost=mariadb:3306 \
+    --path=/srv/www/wordpress
 
 wp core install --allow-root \
     --url=zel-yama.42.fr \
     --title="the my wordpress site" \
     --admin_email=$EMAIL_USER_WORDPRESS \
     --admin_user=$WORDPRESS_ADMIN_USER_NAME \
-    --admin_password=$WORDPRESS_PASSWORD
-
+    --admin_password=$WORDPRESS_PASSWORD \
+    --path=/srv/www/wordpress
 
 wp user create --allow-root \
     $WORDPRESS_USER \
     $WORDPRESS_EMAIL_USER \
     --role=editor \
-    --user_pass=$WORDPRESS_USER_PASSWORD
+    --user_pass=$WORDPRESS_USER_PASSWORD \
+    --path=/srv/www/wordpress 
 
 fi
 
