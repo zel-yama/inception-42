adfa3aa03271d702b23fc31dec73fd5d86de9ebc
zel-yama <138831751+zel-yama@users.noreply.github.com>
2026-06-04 10:59:13 +0000

    my erro came from code space he can let you create net work

--- srcs/requirements/wordpress/tools/setUp.sh
+++ srcs/requirements/wordpress/tools/setUp.sh
@@ -5,31 +5,37 @@ cd /srv/www/wordpress/
 
 sed -i  "s/listen = .*/listen = 9000/g" /etc/php/8.2/fpm/pool.d/www.conf;
 
-#until ping -c1 mariadb  ; do 
- #   echo "hello "
-#  sleep 2
+# until ping -c1 mariadb  ; do 
+#    echo "hello "
+#     sleep 2
 #  done
+#  sleep 10
 until mariadb -hmariadb -u$MYSQL_USER -p$MYSQL_PASSWORD -e "SELECT 1"
 do
     echo "is data base ready " 
     sleep 2
 done
-rm wp-config.php
+
 if [ ! -f wp-config.php ]; then
 
+mv wp-config-sample.php wp-config.php 
 
+wp core download --allow-root 
+#sed -i -r "s/database_name_here/${DATABASE_NAME}/g" wp-config.php
+sed -i -r "s/username_here/${MYSQL_USER}/g" wp-config.php
+sed -i -r "s/password_here/${MYSQL_PASSWORD}/g" wp-config.php
+sed -i -r "s/localhost/mariadb:3306/g" wp-config.php
 
-#wp core download --allow-root 
 # connection refused # wp-config-sample.php  
 # and  wp-config.ph is not create config wordpress is not correct 
-echo "here create config " 
+# echo "here create config " 
 
-wp config create --allow-root \
-    --dbname=$DATABASE_NAME \
-    --dbuser=$MYSQL_USER \
-    --dbpass=$MYSQL_PASSWORD \
-    --dbhost=mariadb:3306 \
-    --path=/srv/www/wordpress
+# wp config create --allow-root \
+#     --dbname=$DATABASE_NAME \
+#     --dbuser=$MYSQL_USER \
+#     --dbpass=$MYSQL_PASSWORD \
+#     --dbhost=mariadb:3306 \
+#     --path=/srv/www/wordpress
 
 wp core install --allow-root \
     --url=zel-yama.42.fr \
@@ -46,6 +52,6 @@ wp user create --allow-root \
     --user_pass=$WORDPRESS_USER_PASSWORD \
     --path=/srv/www/wordpress 
 
-fi
+#fi
 
 exec php-fpm8.2 -F
