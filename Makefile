3dd9d4ee6b2628dff400855b64524c3976dfc41f
zakaria <zakaria@debian.ziko>
2026-06-08 09:52:35 -0400

    set mariadb in good way fixe some problems

---
+++ Makefile
@@ -0,0 +1,7 @@
+
+up:
+	docker compose up -d 
+down:
+	docker compose down  --no-cache 
+build:
+	docker compose build --no-cache
--- srcs/requirements/wordpress/tools/setUp.sh
+++ srcs/requirements/wordpress/tools/setUp.sh
@@ -7,11 +7,11 @@ cd /srv/www/wordpress/
 sed -i  "s/listen = .*/listen = 9000/g" /etc/php/8.2/fpm/pool.d/www.conf;
 
 sleep 5
-# until mysqladmin -h mariadb -u$MYSQL_USER  ping   ; do 
-#    echo "hello "
-#     sleep 2
-#  done
-
+until mysqladmin -h mariadb -u$MYSQL_USER -p$MYSQL_PASSWORD ping   ; do 
+   echo "hello "
+    sleep 2
+ done
+rm wp-config.php
 if [ ! -f wp-config.php ]; then
 
 #mv wp-config-sample.php wp-config.php 
@@ -32,7 +32,7 @@ wp config create --allow-root \
     --dbpass=$MYSQL_PASSWORD \
     --dbhost=mariadb:3306 \
     --path=/srv/www/wordpress
-
+cat wp-config.php 
 wp core install --allow-root \
     --url=zel-yama.42.fr \
     --title="the my wordpress site" \
